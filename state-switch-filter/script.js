6d5912c2f07dbc09861c8e98fff6655c65521829
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-02 09:34:27 +0200

    commit

---
+++ state-switch-filter/script.js
@@ -0,0 +1,32 @@
+﻿class Languages extends Component{
+  state = {
+    array: [...Data]
+  }
+  filterHandler = (event) => {
+    let lang = event.target.getAttribute('lang');
+    switch (lang) {
+      case 'polish': {
+        this.setState({
+          array: [...Data].filter((array) => array.lang === 'polish')
+        });
+        break;
+      }
+      case 'english': {
+        this.setState({
+          array: [...Data].filter((array) => array.lang === 'english')
+        });
+        break;
+      }
+      case 'french': {
+        this.setState({
+          array: [...Data].filter((arrya) => array.lang === 'english')
+        });
+        break;
+      }
+      default: {
+        this.setState({array: [...Data]});
+        break;
+      }
+    }
+  }
+}
